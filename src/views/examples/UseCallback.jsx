76abd7bae235dd32e0d964f8870f8a6f662daebb
WellysonP <wellysonpontes@gmail.com>
2022-12-15 17:53:14 -0300

    finalizado aulas de useState(), useEffect(), useRef(), useMemo(), useCallBack() e useContext()... pendências de useReducer e artificios para criar o proprior use...

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import UseCallBackButtons from './UseCallbackButtons'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseCallback = (props) => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function inc(delta) {
+        setCount(curr =>curr + delta)
+    },[setCount])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallBackButtons inc={inc}/>
+            </div>
         </div>
     )
 }
